d3165a810b57e96e2adc6496829b05d36592b33e
idergachova <dergachovaira@gmail.com>
2023-08-02 17:19:14 +0200

    init folder

---
+++ script.js
@@ -0,0 +1,10 @@
+function navToggle() {
+	document.getElementById("headerUl").classList.toggle("_active");
+}
+
+$('.owl-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:true,
+    margin:10
+});
